3ea7c7a2f0aa8f6702969915c2bdb13e0e087c8a
Kazuya Takei <myself@attakei.net>
2024-03-24 03:00:26 +0900

    doc: Change default language on document

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ intersphinx_mapping = {
 todo_include_todos = True
 
 # atsphinx.mini18n
-mini18n_default_language = "ja"
+mini18n_default_language = "en"
 mini18n_support_languages = ["en", "ja"]
 mini18n_basepath = "/mini18n/"
 
