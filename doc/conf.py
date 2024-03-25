3e1503a581e1e1eb99d4cf61d0d59d87978e85e3
Kazuya Takei <myself@attakei.net>
2024-03-25 13:22:25 +0900

    doc: Add link to GitHub

--- doc/conf.py
+++ doc/conf.py
@@ -26,6 +26,21 @@ locale_dirs = ["_locales"]
 html_theme = "furo"
 html_static_path = ["_static"]
 html_title = f"{project} v{release}"
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+]
+html_theme_options = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/atsphinx/mini18n",
+            "html": "",
+            "class": "fa-brands fa-solid fa-github fa-2x",
+        },
+    ],
+}
 html_sidebars = {
     "**": [
         "sidebar/scroll-start.html",
