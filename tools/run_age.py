5b0089ac868a22cb3b22c43e7c31a2a55895c5ed
Kazuya Takei <myself@attakei.net>
2024-05-16 00:11:11 +0900

    feat(tools): Combine archiving changelog and calling age-cli

---
+++ tools/run_age.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Wapper to ``age`` CLI.
+
+age cannot copy and move files in updating process.
+"""
+
+import argparse
+import shutil
+from pathlib import Path
+from subprocess import run
+
+from atsphinx.mini18n import __version__ as version
+
+project_root = Path(__file__).parent.parent
+archive_to = project_root / "doc" / "changelogs"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("age_args", nargs="+")  # All arguments are passed to age-cli.
+
+
+def main(args: argparse.Namespace):  # noqa: D103
+    # Move changelog into document to use as old logs.
+    changes_rst = project_root / "CHANGES.rst"
+    shutil.move(changes_rst, archive_to / f"{version}.rst")
+    changes_rst.write_text("TEMPLATE")
+    # Run age.
+    run(["age"] + args.age_args)
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
