a82364f67dd2c002243ad1fe4f7f66347ab17bec
Kazuya Takei <myself@attakei.net>
2024-05-17 02:36:40 +0900

    feat: Delegate cookie management to frontend script
    Refs: #2

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,6 +1,36 @@
 /**
  * Module to manage cross-move into i18n sites.
  */
+import Cookies from "js-cookie";
 import * as packageJson from "../../package.json";
+
+const COOKIE_SELECTED_LANG = "lang";
+const COOKIE_MAX_AGE_DAYS = 400;
+
+/**
+ * Redirect page of selected language.
+ */
+const redirectWithRemember = (lang: string, url: string) => {
+  Cookies.set(COOKIE_SELECTED_LANG, lang, {
+    sameSite: "strict",
+    path: "/",
+    expires: new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * COOKIE_MAX_AGE_DAYS,
+    ),
+  });
+  location.href = url;
+};
+
+/**
+ * Handlers for HTML
+ */
+export namespace Widget {
+  export const redirectBySelect = (e: Event) => {
+    //
+    const langCode = e.target.value;
+    const langUrl = e.target.selectedOptions[0].dataset.url;
+    redirectWithRemember(langCode, langUrl);
+  };
+}
 
 console.debug(`Current version is ${packageJson.version}`);
